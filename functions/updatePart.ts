33964f6a945cd45eb77c514583884cd56c5376be
Yoel Torfstein <yoel.robotreat@gmail.com>
2026-02-21 13:32:17 +0000

    File changes

--- functions/updatePart.ts
+++ functions/updatePart.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 
 // Helper to check if value actually changed
 const hasChanged = (oldVal, newVal) => {
