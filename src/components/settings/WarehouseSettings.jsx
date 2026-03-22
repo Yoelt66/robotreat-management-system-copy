cd757c3239a2a4a47c7c23259b8b09f32d33a32b
Yoel Torfstein <yoel.robotreat@gmail.com>
2026-03-22 09:49:44 +0000

    File changes

--- src/components/settings/WarehouseSettings.jsx
+++ src/components/settings/WarehouseSettings.jsx
@@ -33,6 +33,7 @@ export default function WarehouseSettings() {
   const [error, setError] = useState("");
   const [warehouseToDelete, setWarehouseToDelete] = useState(null);
   const [isCreating, setIsCreating] = useState(false);
+  const warehouseToDeleteRef = React.useRef(null);
 
   useEffect(() => {
     loadData();
